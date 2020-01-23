1e87efa60f8377451ed3ce88275fd32f0df829b3
Bhawna Jaswani <bhawnajaswani@Bhawnas-MacBook-Air.local>
2020-01-23 15:55:39 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+# Thsi is it 
+
+pizza = "This is very nice"
+age = 43
+""" 
+number = 23.45
+print("Value for pizza: ",pizza)
+print("Age------------>", age)
+print("Number------------>", number) 
+"""
+
+# if  statement to show dieerent colors
+
+color = "red"
+
+if color == "red":
+    print("The color is red")
+elif color =="green":
+    print("the color is green")
+elif color =="purple":
+    print("The color is purple")
+else:
+    print("The color cant be found")
